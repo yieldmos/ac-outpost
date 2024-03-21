7092ad7b3904b9cd69eec14cbfd2dfc3c4df45e2
Obie Kaku <obiekaku@gmail.com>
2024-03-21 16:53:59 -0500

    updated osmodca contract to deal with the take rates

--- contracts/osmodca/src/state.rs
+++ contracts/osmodca/src/state.rs
@@ -1,12 +1,14 @@
 use cosmwasm_std::Addr;
 use cw_storage_plus::{Item, Map};
 use osmosis_destinations::pools::{StoredDenoms, StoredPools};
+use outpost_utils::comp_prefs::TakeRate;
 
 use crate::msg::ContractAddrs;
 
 pub const ADMIN: Item<Addr> = Item::new("admin");
 pub const AUTHORIZED_ADDRS: Item<Vec<Addr>> = Item::new("allowed_addrs");
 pub const PROJECT_ADDRS: Item<ContractAddrs> = Item::new("project_addrs");
+pub const TAKE_RATE: Item<TakeRate> = Item::new("take_rate");
 
 pub const KNOWN_OSMO_POOLS: StoredPools = Map::new("known_osmo_pools");
 pub const KNOWN_USDC_POOLS: StoredPools = Map::new("known_usdc_pools");
